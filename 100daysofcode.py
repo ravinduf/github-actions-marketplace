3c8c166079d0f0d3624b8b7c7532613f54607899
ravinduf <ravinduf@rocketmail.com>
2022-04-26 09:14:49 +0530

    added error

--- 100daysofcode.py
+++ 100daysofcode.py
@@ -4,7 +4,7 @@ start = date(2020, 1, 1)
 today = date.today()
 delta = today - start
 
-if (delta.days < 101):
+if (delta.days <? 101):
     print("Today is day {}".format(delta.days))
 else:
     print('100 Days of Code sprint has ended')
